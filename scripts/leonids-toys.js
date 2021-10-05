cf8f5a5926b31905d189cacbe2cbfe989d76ff3f
bytesparallel <jason.ross841@gmail.com>
2021-10-05 15:01:01 -0500

    backup first object

---
+++ scripts/leonids-toys.js
@@ -0,0 +1,11 @@
+const buildingBlocks = {
+    id: 1
+    name: "building blocks",
+    type: "construction toy",
+    price: `${10.00}`,
+    stock status: true,
+    item description: "High quality elm wood blocks perfect kids 2-5 years old to help develop motor skills and to develop creativity!",
+    manufacturer: "Hasboro"
+
+}
+
